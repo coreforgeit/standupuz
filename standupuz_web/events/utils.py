8e143a49749c58e237da307120d87f13390a7b9a
dgush <dgushch@gmail.com>
2024-06-25 16:23:27 +0400

    0.0.11

--- standupuz_web/events/utils.py
+++ standupuz_web/events/utils.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from standupuz_web.settings import MEDIA_ROOT
@@ -9,4 +10,6 @@ def get_photo_url(photo_id: str) -> str:
     path = os.path.join(MEDIA_ROOT, f'{photo_id}.jpg')
     if not os.path.exists(path):
         download_file(photo_id, path)
-    return f'media/{photo_id}.jpg'
+    return f'../media/{photo_id}.jpg'
+
+
--- standupuz_web/standupuz_web/urls.py
+++ standupuz_web/standupuz_web/urls.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls.static import static
 
 from standupuz_web import settings
-from events.views import events_view, about_view
+from events import views
+
+import re
 
 urlpatterns = [
-    path('', events_view, name='events'),
-    path('events/', events_view, name='events'),
-    path('events_mob/', events_view, name='events'),
-    path('about/', about_view, name='about'),
+    path('', views.home_page_redirect, name='home'),
+    path('events/', views.events_view, name='events'),
+    path('event_mob/<int:event_id>/', views.event_mob_view, name='event_mob'),
+    path('about/', views.about_view, name='about'),
     path('admin/', admin.site.urls),
 ]
 
