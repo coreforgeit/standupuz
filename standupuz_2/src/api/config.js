7c283e6d974968f57c4241ad0aebbb399f84b728
dgush <dgushch@gmail.com>
2025-05-13 16:19:34 +0400

    1.0.12

--- standupuz_2/src/api/config.js
+++ standupuz_2/src/api/config.js
@@ -1,7 +1,7 @@
 // src/api/config.js
 
 // 1) Сначала пробуем взять из .env, иначе — дефолт localhost:
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://standupcomedy.uz/';
 
 // 2) Перечисляем все пути вашего API
 export const API_PATHS = {
