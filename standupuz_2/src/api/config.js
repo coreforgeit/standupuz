5781daea6195c485bc783510d404f542c54b4ac3
dgush <dgushch@gmail.com>
2025-05-13 17:35:29 +0400

    1.0.17

--- standupuz_2/src/api/config.js
+++ standupuz_2/src/api/config.js
@@ -6,6 +6,6 @@ export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || ORIGIN;
 
 // 2) Перечисляем все пути вашего API
 export const API_PATHS = {
-  events: 'api/event/',
-  info:   'api/info/',
+  events: '/api/event/',
+  info:   '/api/info/',
 };
