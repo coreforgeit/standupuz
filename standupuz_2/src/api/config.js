3782ffe2dd5a0627ab82aaaa7c21f5218ee95f00
dgush <dgushch@gmail.com>
2025-05-14 13:51:10 +0400

    1.0.24

--- standupuz_2/src/api/config.js
+++ standupuz_2/src/api/config.js
@@ -1,8 +1,8 @@
 // src/api/config.js
 
 // 1) Сначала пробуем взять из .env, иначе — дефолт localhost:
-// export const API_BASE_URL = window.location.origin;
-export const API_BASE_URL = 'http://localhost';
+export const API_BASE_URL = window.location.origin;
+// export const API_BASE_URL = 'http://localhost';
 
 // 2) Перечисляем все пути вашего API
 export const API_PATHS = {
