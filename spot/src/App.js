e7383a35a8525cb36ae0a6e08f4d4bf4d46ecb39
dgush <dgushch@gmail.com>
2025-06-15 15:19:06 +0400

    1.1.16

--- spot/src/App.js
+++
@@ -1,9 +0,0 @@
-// src/App.jsx
-import React from 'react';
-import Home from './pages/Home/index.jsx';  // или './pages/Home/index.jsx'
-
-function App() {
-  return <Home />;
-}
-
-export default App;
